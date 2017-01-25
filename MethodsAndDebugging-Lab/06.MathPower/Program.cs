1bd929df2a2966f61e1bcbe853d8f6738d5acbc8
ivan peev <ivan.boykov.peev@gmail.com>
2017-01-25 20:16:23 +0200

    Add 05.CalculateTriangleArea and 06.MathPower

---
+++ MethodsAndDebugging-Lab/06.MathPower/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06.MathPower
+{
+    class Program
+    {
+        static void Main()
+        {
+            double number = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+            double result = RaiseToPower(number, power);
+            Console.WriteLine(result);
+        }
+
+        private static double RaiseToPower(double number, double power)
+        {
+            double result = number;
+
+            for (int i = 1; i < power; i++ )
+            {
+                result = result * number;
+            }
+
+            return result;
+        }
+    }
+}
