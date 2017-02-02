09467b5e622ae0a678de49bb5affdcf4834768ff
ivan boykov peev <ivan.boykov.peev@gmail.com>
2017-02-02 11:28:04 +0200

    Add solution to 01.HelloName and 02.MaxMethod

---
+++ MethodsAndDebugging-Exercises/01.HelloName/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01.HelloName
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            PrintGriiting(name);
+        }
+
+        private static void PrintGriiting(string name)
+        {
+            Console.WriteLine("Hello, {0}!", name);
+        }
+    }
+}
---
+++ MethodsAndDebugging-Exercises/02.MaxMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02.MaxMethod
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int max = GetMax(GetMax(a, b), c);
+            Console.WriteLine(max);
+        }
+
+        private static int GetMax(int a, int b)
+        {
+            if (a >= b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+    }
+}
